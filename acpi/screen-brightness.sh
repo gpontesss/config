73ecb4fb6493c07f66dfb6c20bcae165285ed991
gpontesss <pontes.guisilva@gmail.com>
2021-03-27 21:16:40 -0300

    Refactor Makefile and add acpi files

--- acpi/screen-brightness.sh
+++ acpi/screen-brightness.sh
@@ -10,20 +10,19 @@ usage() {
 
 BASE_PATH=/sys/class/backlight/intel_backlight
 MAX_BRIGHTNESS=$(cat $BASE_PATH/max_brightness)
-ACTUAL_BRIGHTNESS=$(cat $BASE_PATH/brightness)
+CURRENT_BRIGHTNESS=$(cat $BASE_PATH/brightness)
 
 case $1 in
 	"inc")
-		NEW_VALUE=$(expr $ACTUAL_BRIGHTNESS + $2)
+		NEW_VALUE=$(expr $CURRENT_BRIGHTNESS + $2)
 		[[ $NEW_VALUE -gt $MAX_BRIGHTNESS ]] && NEW_VALUE=$MAX_BRIGHTNESS
 		echo $NEW_VALUE
 		;;
 	"dec")
-		NEW_VALUE=$(expr $ACTUAL_BRIGHTNESS - $2) 
+		NEW_VALUE=$(expr $CURRENT_BRIGHTNESS - $2)
 		[[ 0 -gt $NEW_VALUE ]] && NEW_VALUE=0
 		;;
 	*) usage ;;
 esac
 
 echo $NEW_VALUE > $BASE_PATH/brightness || usage
-
